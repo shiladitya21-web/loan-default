4c1cba952d205458e98f576e2819ef6e07d5dc63
shiladitya21-web <119227552+shiladitya21-web@users.noreply.github.com>
2022-11-30 01:18:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "[server]"  > ~/.streamlit/config.toml 
-echo "headless = true"  >> ~/.streamlit/config.toml
-echo "port = $PORT"  >> ~/.streamlit/config.toml
-echo "enableCORS = false"  >> ~/.streamlit/config.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
